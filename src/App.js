da11737966196c60481b5ae83b6bd107a9ff5ea7
Divyanshu Tiwari <tdivyanshu99@gmail.com>
2021-07-01 23:39:29 +0530

    done

--- src/App.js
+++ src/App.js
@@ -5,8 +5,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        Welcome!
-        <p>This is Divyanshu Tiwari</p>
+        Welcome to my portfolio website!
       </header>
     </div>
   );
